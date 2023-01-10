9199cd1dc90562eddd25949bdf6558fc8f920c46
Sergio Alves <salves@Sergios-MacBook-Pro.local>
2023-01-10 15:17:23 +0100

    Add controller advice for not handled exceptions

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,14 +35,14 @@ public class ElasticDocumentController {
     @GetMapping("/{id}")
     public @ResponseBody
     ResponseEntity<ElasticQueryServiceResponseModel>
-    getDocumentById(@PathVariable String id) {
+    getDocumentById(@PathVariable @NotEmpty String id) {
         return ResponseEntity.ok(elasticQueryService.getDocumentsById(id));
     }
 
     @PostMapping("/get-document-by-text")
     public @ResponseBody
     ResponseEntity<List<ElasticQueryServiceResponseModel>>
-    getDocumentByText(@RequestBody ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
+    getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
         List<ElasticQueryServiceResponseModel> documentsByText = elasticQueryService.getDocumentsByText(elasticQueryServiceRequestModel.getText());
         LOG.info("Elasticsearch returned {} of documents", documentsByText.size());
         return ResponseEntity.ok(documentsByText);
